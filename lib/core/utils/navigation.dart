8412a682bd72ec4181f879a7c52660077661321b
AhmedElesawy <elesawy928@gmail.com>
2025-07-24 01:45:09 +0300

    navigation extenstion

---
+++ lib/core/utils/navigation.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+extension Navigation on BuildContext{
+
+  void push(Widget page) {
+    Navigator.push(
+      this,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
+
+  void pushReplacement(Widget page) {
+    Navigator.pushReplacement(
+      this,
+      MaterialPageRoute(builder: (context) => page),
+    );
+  }
+
+  void pop() {
+    Navigator.pop(this);
+  }
+}
