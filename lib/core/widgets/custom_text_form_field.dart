ab27da4e423ad5b834b3f37f73c375fc7a1e110c
AhmedElesawy <elesawy928@gmail.com>
2025-07-24 03:03:15 +0300

    refactor custom_text_form_field

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -1,27 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class CustomTextFormField extends StatelessWidget {
+class CustomTextFormField extends StatefulWidget {
   const CustomTextFormField({
     super.key,
     this.controller,
     required this.hintText,
     required this.labelText,
     this.validator,
+    this.obscureField,
   });
   final TextEditingController? controller;
   final String hintText;
   final String labelText;
   final String? Function(String?)? validator;
+  final bool? obscureField;
+
+  @override
+  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
+}
+
+class _CustomTextFormFieldState extends State<CustomTextFormField> {
+  bool isObscure = false;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      controller: controller,
+      controller: widget.controller,
       cursorColor: Colors.white,
-      validator: validator,
+      validator: widget.validator,
       decoration: InputDecoration(
-        hintText: hintText,
-        labelText: labelText,
+        hintText: widget.hintText,
+        labelText: widget.labelText,
 
         labelStyle: const TextStyle(color: Colors.white),
         border: outlineInputBorderField(),
@@ -29,7 +38,22 @@ class CustomTextFormField extends StatelessWidget {
         focusedBorder: outlineInputBorderField(width: 1.5.w),
         enabledBorder: outlineInputBorderField(),
         errorBorder: outlineInputBorderField(color: Colors.red),
+
+        suffixIcon: widget.obscureField == true
+            ? IconButton(
+                icon: Icon(
+                  isObscure ? Icons.visibility : Icons.visibility_off,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  setState(() {
+                    isObscure = !isObscure;
+                  });
+                },
+              )
+            : null,
       ),
+      obscureText: widget.obscureField == true ? isObscure : false,
     );
   }
 
