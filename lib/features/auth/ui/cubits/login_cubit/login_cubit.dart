41d3cf1902d6dba8dd70e36abcc6ced6c8ace8e0
AhmedElesawy <elesawy928@gmail.com>
2025-07-25 03:32:36 +0300

    create login cubit

---
+++ lib/features/auth/ui/cubits/login_cubit/login_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
+import 'package:meta/meta.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
+  Future<void> login() async {
+    emit(LoginLoading());
+    try {
+      await auth.signInWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      emit(LoginSuccess());
+    } catch (e) {
+      emit(LoginError(e.toString()));
+    }
+  }
+}
