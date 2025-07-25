9bd6c900ad9dfe910027eb5a3e7fff9185064bb0
AhmedElesawy <elesawy928@gmail.com>
2025-07-25 03:35:56 +0300

    create signup cubit

---
+++ lib/features/auth/ui/cubits/signup_cubit/sign_up_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit() : super(SignUpInitial());
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
+  Future<void> signup() async {
+    emit(SignUpLoading());
+    try {
+      await auth.createUserWithEmailAndPassword(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+      emit(SignUpSuccess());
+    } catch (e) {
+      emit(SignUpError(e.toString()));
+    }
+  }
+}
